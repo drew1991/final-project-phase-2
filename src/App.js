989cf1b2f529f55e7f7e67bab303712d12af4305
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-10-08 11:22:46 +0300

    styling pagination some more

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-// import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Navbar from "./components/layout/Navbar";
-import { Routes, Route } from "react-router";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
 import AddUser from "./components/users/AddUser";
 import EditUser from "./components/users/EditUser";
 import User from "./components/users/User";
+import { useState } from "react";
+import Pagination from "./components/pagination/pagination";
 
 function App(props) {
   const [currentPage, setCurrentPage] = useState(1);
   return (
-    <Routes>
+    <Router>
       <Navbar />
 
-      <Routes>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/users/add" component={AddUser} />
         <Route exact path="/users/edit/:id" component={EditUser} />
         <Route exact path="/users/:id" component={User} />
-      </Routes>
-    </Routes>
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
