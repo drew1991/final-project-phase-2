d3259ddc1c3dcdf9c31f63744ebac14c09f7606b
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-10-08 13:52:54 +0300

    Updates

---
+++ precache-manifest.e3076995118ad63068a3b28b842760d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4713d0eef67e5b00e5ab",
+    "url": "/final-project-phase-2/static/css/main.d420c7c8.chunk.css"
+  },
+  {
+    "revision": "4713d0eef67e5b00e5ab",
+    "url": "/final-project-phase-2/static/js/main.4713d0ee.chunk.js"
+  },
+  {
+    "revision": "b01aff764c6adae9a6a7",
+    "url": "/final-project-phase-2/static/css/1.42ed92a0.chunk.css"
+  },
+  {
+    "revision": "b01aff764c6adae9a6a7",
+    "url": "/final-project-phase-2/static/js/1.b01aff76.chunk.js"
+  },
+  {
+    "revision": "cc273a5b4668fe90a6d5",
+    "url": "/final-project-phase-2/static/js/runtime~main.cc273a5b.js"
+  },
+  {
+    "revision": "7652b00fadadd7811a2d469a4a618b94",
+    "url": "/final-project-phase-2/static/media/bg.7652b00f.jpg"
+  },
+  {
+    "revision": "e16c410394609ecf79c7ebd8a93f9bef",
+    "url": "/final-project-phase-2/static/media/2.e16c4103.jpg"
+  },
+  {
+    "revision": "f34d8e0806711be81df33e4af89b6003",
+    "url": "/final-project-phase-2/static/media/montez-latin-400-normal.f34d8e08.woff2"
+  },
+  {
+    "revision": "8eae0f196d0eed87cd310d5a511b3696",
+    "url": "/final-project-phase-2/static/media/montez-all-400-normal.8eae0f19.woff"
+  },
+  {
+    "revision": "6ca20f7a3819f6617dd293b3ba72fc25",
+    "url": "/final-project-phase-2/index.html"
+  }
+];
